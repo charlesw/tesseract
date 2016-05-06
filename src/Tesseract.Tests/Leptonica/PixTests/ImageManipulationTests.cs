fb2bd583fd615d7257fdded53bac2f98810280ab
nguyenq <nguyenq@users.sf.net>
2016-05-06 18:27:48 -0500

    More Leptonica API functions and implement RemoveLines

--- src/Tesseract.Tests/Leptonica/PixTests/ImageManipulationTests.cs
+++ src/Tesseract.Tests/Leptonica/PixTests/ImageManipulationTests.cs
@@ -12,7 +12,7 @@ namespace Tesseract.Tests.Leptonica.PixTests
     public class ImageManipulationTests
     {
         const string ResultsDirectory = @"Results\ImageManipulation\";
-        
+
         [Test]
         public void DescewTest()
         {
@@ -23,7 +23,7 @@ namespace Tesseract.Tests.Leptonica.PixTests
                 {
                     Assert.That(scew.Angle, Is.EqualTo(-9.953125F).Within(0.00001));
                     Assert.That(scew.Confidence, Is.EqualTo(3.782913F).Within(0.00001));
-                    
+
                     SaveResult(descewedImage, "descewedImage.png");
                 }
             }
@@ -46,9 +46,12 @@ namespace Tesseract.Tests.Leptonica.PixTests
         [Test]
         public void SauvolaBinarizationTest()
         {
-            using (var sourcePix = Pix.LoadFromFile(@".\Data\Binarization\neo-8bit-grayscale.png")) {
-                using (var grayscalePix = sourcePix.ConvertRGBToGray(1, 1, 1)) {
-                    using (var binarizedImage = grayscalePix.BinarizeSauvola(10, 0.35f, false)) {
+            using (var sourcePix = Pix.LoadFromFile(@".\Data\Binarization\neo-8bit-grayscale.png"))
+            {
+                using (var grayscalePix = sourcePix.ConvertRGBToGray(1, 1, 1))
+                {
+                    using (var binarizedImage = grayscalePix.BinarizeSauvola(10, 0.35f, false))
+                    {
                         Assert.That(binarizedImage, Is.Not.Null);
                         Assert.That(binarizedImage.Handle, Is.Not.EqualTo(IntPtr.Zero));
                         SaveResult(binarizedImage, "binarizedSauvolaImage.png");
@@ -60,9 +63,12 @@ namespace Tesseract.Tests.Leptonica.PixTests
         [Test]
         public void SauvolaTiledBinarizationTest()
         {
-            using (var sourcePix = Pix.LoadFromFile(@".\Data\Binarization\neo-8bit-grayscale.png")) {
-                using (var grayscalePix = sourcePix.ConvertRGBToGray(1, 1, 1)) {
-                    using (var binarizedImage = grayscalePix.BinarizeSauvolaTiled(10, 0.35f, 2, 2)) {
+            using (var sourcePix = Pix.LoadFromFile(@".\Data\Binarization\neo-8bit-grayscale.png"))
+            {
+                using (var grayscalePix = sourcePix.ConvertRGBToGray(1, 1, 1))
+                {
+                    using (var binarizedImage = grayscalePix.BinarizeSauvolaTiled(10, 0.35f, 2, 2))
+                    {
                         Assert.That(binarizedImage, Is.Not.Null);
                         Assert.That(binarizedImage.Handle, Is.Not.EqualTo(IntPtr.Zero));
                         SaveResult(binarizedImage, "binarizedSauvolaTiledImage.png");
@@ -78,10 +84,10 @@ namespace Tesseract.Tests.Leptonica.PixTests
             using (var grayscaleImage = sourcePix.ConvertRGBToGray())
             {
                 Assert.That(grayscaleImage.Depth, Is.EqualTo(8));
-        		SaveResult(grayscaleImage, "grayscaleImage.jpg");
+                SaveResult(grayscaleImage, "grayscaleImage.jpg");
             }
-        }        
-        
+        }
+
         [Test]
         [TestCase(45)]
         [TestCase(80)]
@@ -90,40 +96,83 @@ namespace Tesseract.Tests.Leptonica.PixTests
         [TestCase(270)]
         public void Rotate_ShouldBeAbleToRotateImageByXDegrees(float angle)
         {
-        	const string FileNameFormat = "rotation_{0}degrees.jpg";
-        	float angleAsRadians = (float)(angle * Math.PI / 180.0f);
-        	using (var sourcePix = Pix.LoadFromFile(@".\Data\Conversion\photo_rgb_32bpp.tif")) {
-        		using (var result = sourcePix.Rotate(angleAsRadians, RotationMethod.AreaMap))
-	            {
-        			// TODO: Visualy confirm successful rotation and then setup an assertion to compare that result is the same.
-        			var filename = String.Format(FileNameFormat, angle);
-        			SaveResult(result, filename);
-	            }
-        	}
+            const string FileNameFormat = "rotation_{0}degrees.jpg";
+            float angleAsRadians = (float)(angle * Math.PI / 180.0f);
+            using (var sourcePix = Pix.LoadFromFile(@".\Data\Conversion\photo_rgb_32bpp.tif"))
+            {
+                using (var result = sourcePix.Rotate(angleAsRadians, RotationMethod.AreaMap))
+                {
+                    // TODO: Visualy confirm successful rotation and then setup an assertion to compare that result is the same.
+                    var filename = String.Format(FileNameFormat, angle);
+                    SaveResult(result, filename);
+                }
+            }
         }
 
+        [Test]
+        public void RemoveLinesTest()
+        {
+            using (var sourcePix = Pix.LoadFromFile(@".\Data\processing\table.png"))
+            {
+                // remove horizontal lines
+                using (var result = sourcePix.RemoveLines())
+                {
+                    // rotate 90 degrees cw
+                    using (var result1 = result.Rotate90(1))
+                    {
+                        // effectively remove vertical lines
+                        using (var result2 = result1.RemoveLines())
+                        {
+                            // rotate 90 degrees ccw
+                            using (var result3 = result2.Rotate90(-1))
+                            {
+                                // TODO: Visualy confirm successful rotation and then setup an assertion to compare that result is the same.
+                                SaveResult(result3, "tableBordersRemoved.png");
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        [Test]
+        public void Rotate90Test()
+        {
+            using (var sourcePix = Pix.LoadFromFile(@".\Data\Conversion\photo_palette_8bpp.png"))
+            {
+                // rotate 90 degrees cw
+                using (var result = sourcePix.Rotate90(1))
+                {
+                    // TODO: Visualy confirm successful rotation and then setup an assertion to compare that result is the same.
+                    SaveResult(result, "imageRotated90.png");
+                }
+            }
+        }
 
         [Test]
         public void Scale_RGB_ShouldBeScaledBySpecifiedFactor(
             [Values(0.25f, 0.5f, 0.75f, 1, 1.25f, 1.5f, 1.75f, 2, 4, 8)]  float scale)
         {
-        	const string FileNameFormat = "scale_{0}.jpg";
-            using (var sourcePix = Pix.LoadFromFile(@".\Data\Conversion\photo_rgb_32bpp.tif")) {
-                using (var result = sourcePix.Scale(scale, scale)) {
+            const string FileNameFormat = "scale_{0}.jpg";
+            using (var sourcePix = Pix.LoadFromFile(@".\Data\Conversion\photo_rgb_32bpp.tif"))
+            {
+                using (var result = sourcePix.Scale(scale, scale))
+                {
                     Assert.That(result.Width, Is.EqualTo((int)Math.Round(sourcePix.Width * scale)));
                     Assert.That(result.Height, Is.EqualTo((int)Math.Round(sourcePix.Height * scale)));
 
-        			// TODO: Visualy confirm successful rotation and then setup an assertion to compare that result is the same.
-        			var filename = String.Format(FileNameFormat, scale);
-        			SaveResult(result, filename);
+                    // TODO: Visualy confirm successful rotation and then setup an assertion to compare that result is the same.
+                    var filename = String.Format(FileNameFormat, scale);
+                    SaveResult(result, filename);
                 }
             }
         }
-                      
+
         private void SaveResult(Pix result, string filename)
         {
             if (!Directory.Exists(ResultsDirectory)) Directory.CreateDirectory(ResultsDirectory);
-        	
+
             result.Save(Path.Combine(ResultsDirectory, filename));
         }
     }
