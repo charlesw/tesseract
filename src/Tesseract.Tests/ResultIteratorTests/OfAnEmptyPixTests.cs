49da3756f288b68b96d7eb2fc783375cc262f629
Charles Weld <charles.weld@gmail.com>
2017-01-26 11:21:12 +1100

    #308 - Test to confirm workaround for empty iterator
    Since the PageIterator::Empty method isn't available in the api we have to base a test on
    PageIterator::GetUTF8Text method , which is mapped to PageIterator.GetText, instead which
    returns null if there is no content.

---
+++ src/Tesseract.Tests/ResultIteratorTests/OfAnEmptyPixTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesseract.Tests.ResultIteratorTests
+{
+    [TestFixture]
+    public class OfAnEmptyPixTests : TesseractTestBase
+    {
+        private TesseractEngine Engine { get; set; }
+        private Pix EmptyPix { get; set; }
+
+        [SetUp]
+        public void Init()
+        {
+            Engine = CreateEngine();
+            EmptyPix = LoadTestPix("Ocr\\blank.tif");
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            if (EmptyPix != null) {
+                EmptyPix.Dispose();
+                EmptyPix = null;
+            }
+
+            if (Engine != null) {
+                Engine.Dispose();
+                Engine = null;
+            }
+        }
+
+        #region Tests
+
+        [Theory]
+        public void GetTextReturnNullForEachLevel(PageIteratorLevel level)
+        {
+            using (var page = Engine.Process(EmptyPix)) {
+                using (var iter = page.GetIterator()) {
+                    Assert.That(iter.GetText(level), Is.Null);
+                }
+            }
+        }
+
+        #endregion Tests
+    }
+}
