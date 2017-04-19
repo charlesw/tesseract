1c6db0c86dfecdc9723e6723fa8e364ea1b7fd03
Zach Bean <zb@forty2.com>
2017-04-18 23:19:30 -0700

    * Add a FontInfo class that mirrors Tesseract's own FontInfo struct.  This contains the font attributes that are cacheable by ID.  * Add a FontAttributes class to serve as the return value of GetWordFontAttributes()  * Add the necessary glue to actually get the word font attributes from the native side  * Throw in a wrapper for WordRecognitionLanguage() as well since it seems like it might be useful.

--- src/Tesseract/ResultIterator.cs
+++ src/Tesseract/ResultIterator.cs
@@ -28,6 +28,49 @@ namespace Tesseract
             return Interop.TessApi.ResultIteratorGetUTF8Text(handle, level);
         }
         
+        public FontAttributes GetWordFontAttributes() {
+            VerifyNotDisposed();
+            if (handle.Handle == IntPtr.Zero) {
+                return null;
+            }
+
+            bool is_bold, is_italic, is_underlined,
+                    is_monospace, is_serif, is_smallcaps;
+
+            int pointsize, font_id;
+            string name =
+                Interop.TessApi.ResultIteratorWordFontAttributes(
+                    handle,
+                    out is_bold, out is_italic, out is_underlined,
+                    out is_monospace, out is_serif, out is_smallcaps,
+                    out pointsize, out font_id
+                );
+
+            // this can happen in certain error conditions.
+            if (name == null) {
+                return null;
+            }
+
+            var fontInfo =
+                FontInfo.GetOrCreate(
+                    name, font_id,
+                    is_italic, is_bold,
+                    is_monospace, is_serif
+                );
+
+            return new FontAttributes(fontInfo, is_underlined, is_smallcaps, pointsize);
+        }
+
+        public string GetWordRecognitionLanguage()
+        {
+            VerifyNotDisposed();
+            if (handle.Handle == IntPtr.Zero) {
+                return null;
+            }
+
+            return Interop.TessApi.ResultIteratorWordRecognitionLanguage(handle);
+        }
+
         /// <summary>
         /// Gets an instance of a choice iterator using the current symbol of interest. The ChoiceIterator allows a one-shot iteration over the
         /// choices for this symbol and after that is is useless.
