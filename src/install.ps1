0ebfa3bf1ebc739e265ed4c751db4fa9137c2819
nguyenq <nguyenq@users.sf.net>
2018-02-08 12:17:30 -0600

    Update Leptonica 1.75.1

--- src/install.ps1
+++ src/install.ps1
@@ -4,7 +4,7 @@
 param($installPath, $toolsPath, $package, $project)
 
 $platformNames = "x86", "x64"
-$fileNames = "libtesseract400.dll", "liblept1750.dll"
+$fileNames = "libtesseract400.dll", "liblept1751.dll"
 $propertyName = "CopyToOutputDirectory"
 
 foreach($platformName in $platformNames) {
