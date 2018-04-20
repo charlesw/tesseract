097233c3f22de32c4d2db6f3b7d41e700fa6b975
lbl <luc.bologna@p3-group.com>
2018-04-20 14:15:26 +0200

    Use 'readonly' in ElementProperties

--- src/Tesseract/ElementProperties.cs
+++ src/Tesseract/ElementProperties.cs
@@ -9,49 +9,32 @@ namespace Tesseract
 	/// </summary>
     public struct ElementProperties
     {
-        private Orientation orientation;
-        private TextLineOrder textLineOrder;
-        private WritingDirection writingDirection;
-        private float deskewAngle;
-
         public ElementProperties(Orientation orientation, TextLineOrder textLineOrder, WritingDirection writingDirection, float deskewAngle)
         {
-            this.orientation = orientation;
-            this.textLineOrder = textLineOrder;
-            this.writingDirection = writingDirection;
-            this.deskewAngle = deskewAngle;
+            Orientation = orientation;
+            TextLineOrder = textLineOrder;
+            WritingDirection = writingDirection;
+            DeskewAngle = deskewAngle;
         }
 
         /// <summary>
         /// Gets the <see cref="Orientation" /> for corresponding text block.
         /// </summary>
-        public Orientation Orientation
-        {
-            get { return orientation; }
-        }
+        public readonly Orientation Orientation;
 
         /// <summary>
         /// Gets the <see cref="TextLineOrder" /> for corresponding text block.
         /// </summary>
-        public TextLineOrder TextLineOrder
-        {
-            get { return textLineOrder; }
-        }
+        public readonly TextLineOrder TextLineOrder;
 
         /// <summary>
         /// Gets the <see cref="WritingDirection" /> for corresponding text block.
         /// </summary>
-        public WritingDirection WritingDirection
-        {
-            get { return writingDirection; }
-        }
+        public readonly WritingDirection WritingDirection;
 
         /// <summary>
         /// Gets the angle the page would need to be rotated to deskew the text block.
         /// </summary>
-        public float DeskewAngle
-        {
-            get { return deskewAngle; }
-        }
+        public readonly float DeskewAngle;
     }
 }
